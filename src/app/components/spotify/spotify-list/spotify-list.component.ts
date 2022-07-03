62705dde7f0e8febe1cb818849337ae8820abefc
ImanNazirah <nurimannazirah95@gmail.com>
2022-07-03 20:58:06 +0800

    include paginator for listing ,disable footer

--- src/app/components/spotify/spotify-list/spotify-list.component.ts
+++ src/app/components/spotify/spotify-list/spotify-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { PageableResponseModel, ResponseModel, SingleDataResponseModel } from 'src/app/models/response';
 import { Spotify } from 'src/app/models/spotify';
 import { SpotifyService } from 'src/app/services/spotify.service';
+import { Pageable } from 'src/app/models/pageable';
 
 @Component({
   selector: 'app-spotify-list',
@@ -22,6 +24,11 @@ export class SpotifyListComponent implements OnInit {
   displayedColumns: string[] = ['artistName', 'trackName', 'genre'];
   dataSource : MatTableDataSource<Spotify> = new MatTableDataSource();
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  // @ViewChild(MatSort) sort: MatSort;
+
+  pageable : Pageable;
+
   constructor(
     private _spotifyService: SpotifyService
   ) {
@@ -31,6 +38,8 @@ export class SpotifyListComponent implements OnInit {
         // this.queryParam.genre ='Pop';
         // this.queryParam.artistName =undefined;
 
+        this.pageable = new Pageable();
+
         //get data with query
         this._spotifyService.getByQueryData(0,10,this.queryParam)
         .pipe(
@@ -38,8 +47,14 @@ export class SpotifyListComponent implements OnInit {
         )
         .subscribe(x => {
 
+          //to display data in table
           this.viewListing$ = x?.content;
           this.dataSource = new MatTableDataSource(this.viewListing$);
+
+          //to pass pagination 
+          this.pageable.length = x?.totalElements;
+          this.pageable.pageIndex = x?.number;
+          this.pageable.pageSize =  x?.size;
 
         });
 
@@ -47,6 +62,32 @@ export class SpotifyListComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  // ngAfterViewInit() {
+    
+  // }
+
+  public handlePage(e: PageEvent) {
+
+     //get data with query upon clicking mat paginator
+     this._spotifyService.getByQueryData(e.pageIndex,e.pageSize,this.queryParam)
+     .pipe(
+       map((response: PageableResponseModel<Spotify>) => response.data)
+     )
+     .subscribe(x => {
+
+       //to display data in table
+       this.viewListing$ = x?.content;
+       this.dataSource = new MatTableDataSource(this.viewListing$);
+
+       //to pass pagination 
+       this.pageable.length = x?.totalElements;
+       this.pageable.pageIndex = x?.number;
+       this.pageable.pageSize =  x?.size;
+
+     });
+
   }
 
 }
---
+++ src/app/models/pageable.ts
@@ -0,0 +1,6 @@
+export class Pageable {
+
+    length?: number;
+    pageIndex?: number;
+    pageSize?: number;
+}
